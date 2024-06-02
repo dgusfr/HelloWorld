a2177fcb6de4116bc9b89664d99f98c12811d429
dgusfr <diegogustavo.fr@gmail.com>
2024-06-02 11:37:16 -0300

    add responsivity

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -20,7 +20,17 @@ export default function Banner() {
   if (isMobile) {
     return (
       <div className={styles.banner}>
-        <h1 className={styles.titulo}>Olá, Mundo!</h1>
+        <div className={styles.apresentacao}>
+          <h1 className={styles.titulo}>Olá, Mundo!</h1>
+          <p className={styles.paragrafo}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero
+            adipisci quo pariatur fugit, recusandae sed illo. Impedit enim cum
+            ducimus rerum at sed sequi illo! Saepe deserunt debitis pariatur
+          </p>
+        </div>
+        <div className={styles.imagens}>
+          <img className={styles.minhaFoto} src={minhaFoto} alt="Foto" />
+        </div>
       </div>
     );
   }
